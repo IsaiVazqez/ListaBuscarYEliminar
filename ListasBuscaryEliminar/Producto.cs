021fe47f8ee3b1dad798b0cc66b0f188d1e94d7c
IsaiVazqez <92620535+IsaiVazqez@users.noreply.github.com>
2021-10-31 17:05:13 -0600

    Add files via upload

---
+++ ListasBuscaryEliminar/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListasBuscaryEliminar
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public int Cantidad { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
